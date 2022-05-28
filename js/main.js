7fb64829721d8a85c249939a7048dd937bf76f01
krisuv <1krzysztof.bujalski@gmail.com>
2022-05-29 01:21:17 +0200

    creating function unblocking questions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import countries from "./countries.js";
-import { questions } from "./questions.js";
-import { generateRandomSubheading } from "./randomText.js";
+import {questions} from "./questions.js";
+import {generateRandomSubheading} from "./randomText.js";
 
 const resultSection = `
     <section class="results" aria-label="quiz results">
@@ -23,11 +23,12 @@ const resultSection = `
             </div>
         </div>
     </section>`
-   document.querySelector('.results').innerHTML += resultSection
+   // document.querySelector('.results').innerHTML += resultSection
 
 const options = [...document.querySelectorAll('.question__option')]
 const quizSubheading = document.querySelector('.welcome-text__subheading--quiz-subheading')
 const quizForm = document.querySelector('.questions-form')
+const results = document.querySelector('.results')
 
 
 const showResult = () => {
@@ -114,20 +115,55 @@ const createQuestion = ({description, avatar, answers}) => {
 
 questions.forEach(question => createQuestion(question))
 
-// console.log(questions[0])
-// console.log(questions[19])
-
 const submitButton = document.createElement('button')
+const alertMessage = document.createElement('p')
 submitButton.textContent = 'submit'
 submitButton.classList.add('button')
 submitButton.classList.add('button--quiz')
+alertMessage.classList.add('alert-message')
 quizForm.appendChild(submitButton)
+quizForm.appendChild(alertMessage)
 
 // jest problem z totalResult- zawsze na początku ma wartośc true
 
+const countPoints = (checkedAnswers) => {
 
+    let xD = []
+    allAnswers.filter( answer => {
+        checkedAnswers.forEach( checkedAnswer => {
+            if(answer.id === checkedAnswer){
+                xD.push(answer)
+            }
+        })
+    })
+    //console.log(xD)
 
-const requiredAnswers = (event) => {    
+    xD.forEach( answer => {
+        answer.countries.forEach(country => {
+            country.points++
+        })
+    })
+
+    const maxPoints = (countries) => {
+        return countries.reduce((acc, val) => {
+            return acc.points >= val.points ? acc : val
+        })
+    }
+
+    const winnerCountry = maxPoints(countries)
+    let winnerCountries = []
+    winnerCountries = countries.filter(country => country.points === winnerCountry.points)
+    winnerCountries.forEach(winner => console.log(winner))
+
+    //reset all points
+    //zamiast resetu można zablokować przyciski
+    //questionObjects.forEach(questionObject => questionObject.style.filter = `blur(6px)`)
+    //questionsAnswers.forEach(questionAnswer => questionAnswer.style.pointerEvents = `none`)
+
+    //console.log(winnerCountry)
+}
+
+const checkRequiredAnswers = (event) => {
     event.preventDefault();
     let totalResult = true
     const checkedAnswers = []
@@ -139,76 +175,64 @@ const requiredAnswers = (event) => {
         if(result){
             const answerId = (parseInt(result.id))
             checkedAnswers.push(answerId)
-            //let choosenAnswer = null
-
-            // questions.forEach( question => {
-            //     const correctAnswer = question.answers.find( answer => answer.id === answerId)
-            //     if(correctAnswer && totalResult){
-            //         choosenAnswer = correctAnswer
-            //         console.log(choosenAnswer)
-            //         choosenAnswer.countries.forEach( country => {
-            //             ++country.points
-            //         })
-            //     }
-            // })
-            
         } else {
-            event.preventDefault()
-            //countries.forEach(country => country.points = 0)
             totalResult = false
         }
     })
 
     
-    if(!totalResult){
-        console.warn(`You must have forgotten to check the answer in one of the questions 🧿`)
+    if(totalResult){
+        countPoints(checkedAnswers)
+        console.log(countries)
+        alertMessage.textContent = `⚙️`
+        alertMessage.classList.add('waiting')
+        results.innerHTML += resultSection
+        submitButton.style.display = `none`
     } else {
-        //console.log(checkedAnswers)
-
-        let xD = []
-        allAnswers.filter( answer => {
-            checkedAnswers.forEach( checkedAnswer => {
-                if(answer.id === checkedAnswer){
-                    xD.push(answer)
-                }
-            })
-        })
-        //console.log(xD)
-
-        xD.forEach( answer => {
-            answer.countries.forEach(country => {
-                country.points++
-            })
-        })
-        //console.log(countries)
-
-        // function maxValue(...args) {
-        //     const max= args.reduce((acc, val) => {
-        //         return acc > val ? acc : val;
-        //     });
-        //     return max;
-        // }
-        const maxPoints = (countries) => {
-            let maxCountries = []
-            const max = countries.reduce((acc, val) => {
-                if(acc.points === val.points) maxCountries.push(val) 
-                return acc.points >= val.points ? acc : val
-            })
-            if(maxCountries.length){
-                return maxCountries
-            }
-            return max
-        }
-
-        const winnerCountry = maxPoints(countries)
-
-        console.log(winnerCountry)
+        console.warn(`You must have forgotten to check the answer in one of the questions 🧿`)
+        alertMessage.textContent = `You must have forgotten to check the answer in one of the questions 🧿`
+        setTimeout(() => {alertMessage.textContent = ``},2000)
     }
-
-    console.log(countries)
-    showResult()
 
 }
 
+const unblockButtons = () => {
+
+    questionObjects.forEach(questionObject => questionObject.style.filter = `blur(6px)`)
+    questionsAnswers.forEach(questionAnswer => questionAnswer.style.pointerEvents = `none`)
+
+    // questionObjects.forEach((questionObject,index) => {
+    //     if(questionObject)
+    //         })
+}
+
+// questionObjects.slice(1).forEach(questionObject => questionObject.style.filter = `blur(6px)`)
+// questionsAnswers.slice(1).forEach(questionAnswer => questionAnswer.style.pointerEvents = `none`)
+
+
+const unblockButton = (index) => {
+    if(index < questionObjects.length){
+        questionObjects[index].style.filter = `none`
+        questionsAnswers[index].style.pointerEvents = `auto`
+    }
+}
+
+const activateQuestions = () => {
+    let currentQuestion = 1
+    questionsAnswers.forEach(answer => {
+        const options = [...answer.querySelectorAll('.question__option')]
+        const result = options.find(option => option.checked)
+
+        if(result){
+            unblockButton(currentQuestion++)
+        }
+    })
+}
+
+const questionObjects = [...document.querySelectorAll('.question')]
 const questionsAnswers = [...document.querySelectorAll('.question__answers')]
-submitButton.addEventListener('click', requiredAnswers)
+questionObjects.slice(1).forEach(questionObject => questionObject.style.filter = `blur(4px)`)
+questionsAnswers.slice(1).forEach(questionAnswer => questionAnswer.style.pointerEvents = `none`)
+submitButton.addEventListener('click', checkRequiredAnswers)
+questionsAnswers.forEach(questionsAnswer => questionsAnswer.addEventListener('click', activateQuestions))
+console.log(questionsAnswers)
